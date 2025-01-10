40db095db4293c386f82796ab38d8d34c9ff9c7d
João Claudio Bastos Lima <77875291+JoaoClaudioLima@users.noreply.github.com>
2025-01-10 15:23:46 -0300

    Including TASKS routes

--- src/controller/routes/task.py
+++ src/controller/routes/task.py
@@ -1,12 +1,12 @@
 from typing import Optional
 
-from fastapi import Depends, Query, APIRouter
+from fastapi import Depends, Query, APIRouter, HTTPException
 from sqlalchemy.orm import Session
 
-from src.model.database import Tasks
 from src.model.task_schemas import TaskInput
-from src.utils.database import get_from_db, get_db
-from src.utils.task import get_task_from_db
+from src.utils.database import get_db
+from src.utils.task import db_create_task, db_read_task, db_delete_task, db_update_task
+from src.utils.user import db_update_user
 
 tasks_router = APIRouter()
 
@@ -16,19 +16,44 @@ def read_tasks(
         user_id: Optional[int] = Query(None, description="The ID of the user to retrieve"),
         status_id: Optional[int] = Query(None, description="The ID of the status to retrieve"),
         task_id: Optional[int] = Query(None, description="The ID of the task to retrieve"),
-        skip: int = 0,
         db: Session = Depends(get_db)
-    ):
-
+):
     if any([user_id, status_id, task_id]):
-        task = get_task_from_db(db=db, user_id=user_id, status_id=status_id, task_id=task_id)
+        task = db_read_task(db=db, user_id=user_id, status_id=status_id, task_id=task_id)
         return task
 
-    tasks = get_task_from_db(db=db, skip=skip)
+    tasks = db_read_task(db=db)
     return tasks
 
 
 @tasks_router.post("/tasks/", response_model=None, status_code=201)
-def create_user(user: TaskInput, db: Session = Depends(get_db)):
-    # create_new_db_task(user, db)
+def create_task(task: TaskInput, db: Session = Depends(get_db)):
+    db_create_task(task, db)
+
+    #  TODO validar erro de cadastro de status/user que não existem
+
     return "Created"
+
+
+@tasks_router.delete("/tasks/", response_model=None, status_code=200)
+def delete_task(
+        task_id: Optional[int] = Query(None, description="The ID of the task to be deleted"),
+        db: Session = Depends(get_db)):
+    task = db_delete_task(db, task_id)
+    if not task:
+        raise HTTPException(status_code=400, detail="Tarefa não encontrada")
+    return "Tarefa deletada"
+
+
+@tasks_router.put("/tasks/", response_model=None, status_code=200)
+def update_task(
+        task: TaskInput,
+        task_id: Optional[str] = Query(None, description="The ID of the task to be updated"),
+        remove_user: Optional[bool] = Query(False, description="Sets task user_id to None"),
+        db: Session = Depends(get_db)):
+
+    task = db_update_task(db=db, task_input=task, task_id=task_id, remove_user=remove_user)
+
+    if not task:
+        raise HTTPException(status_code=400, detail="Tarefa não pôde ser atualizada")
+    return "Tarefa atualizada"

674ed779023e076409e8c8bbd20d24f1eac38a1c
João Claudio Bastos Lima <77875291+JoaoClaudioLima@users.noreply.github.com>
2025-01-12 12:50:27 -0300

    Container functionality release

--- app.py
+++ app.py
@@ -7,8 +7,12 @@ from src.controller.routes import router
 from src.settings import Settings
 
 
-# Executa as migrações do Alembic
+# Executa as migrações do Alembic.
 alembic_cfg = Config("alembic.ini")
+
+# Faz com que toda vez que o container é gerado, crie uma db nova. Comentar a linha 14 para que os dados persistam.
+command.downgrade(alembic_cfg, "base")
+
 command.upgrade(alembic_cfg, "head")
 
 app = FastAPI(
@@ -27,5 +31,6 @@ app.include_router(router=router, prefix=api_prefix, tags=["FIAP"])
 if __name__ == "__main__":
     uvicorn.run(
         app,
+        host="0.0.0.0",
         port=8123,
     )
